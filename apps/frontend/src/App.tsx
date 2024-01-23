897975f6bec41f78b73508272c4cf0fb48b42921
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-01-23 01:41:15 -0700

    Test full end to end connection

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import './App.css';
+
+const testData = {
+  "name": "Gab",
+  "email": "jimjam@gmail.com",
+  "password": "password",
+  "photo": "12345.jpg",
+  "theme": "Dark",
+  "font": "Sans-Seriff"
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentUser, setCurrentUser] = useState<string>('');
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      return res.text()
+    }).then((data) => {
+      setCurrentUser(data)
+    }).catch((err) => {
+      console.log("The error is: ", err)
+    })
+  }, []);
+
+  const testDatabase = () => {
+    fetch('http://localhost:3000/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(testData)
+    }).then((res) => {
+      console.log("Data saved successfully")
+    }).catch((err) => {
+      console.log("Error saving to database: ", err)
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Username: {currentUser}</h1>
+      <Button onClick={testDatabase}>Click to test database</Button>
     </>
   )
 }
