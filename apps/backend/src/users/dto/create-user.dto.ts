dc4914c8144e71eca7b5fead299e39d18b93e7c1
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-01-23 01:07:35 -0700

    Add entity for user backend and test connection to database

---
+++ apps/backend/src/users/dto/create-user.dto.ts
@@ -0,0 +1,29 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  photo: string;
+
+  @IsNotEmpty()
+  @IsString()
+  theme: string;
+
+  @IsNotEmpty()
+  @IsString()
+  font: string;
+}
