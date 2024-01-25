c39183cadc6fcc959313a55f42bd2ecd04965905
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-01-24 18:24:54 -0700

    Update fetch functions and create GET functionality to test database

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,8 +3,8 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 const testData = {
-  "name": "Gab",
-  "email": "jimjam@gmail.com",
+  "name": "Josh",
+  "email": "jojim@gmail.com",
   "password": "password",
   "photo": "12345.jpg",
   "theme": "Dark",
@@ -12,40 +12,49 @@ const testData = {
 }
 
 function App() {
-  const [currentUser, setCurrentUser] = useState<string>('');
+  const [currentUser, setCurrentUser] = useState([]);
 
-  useEffect(() => {
+  const fetchData = () => {
     fetch('http://localhost:3000/api/users', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
       },
     }).then((res) => {
-      return res.text()
+      return res.json()
     }).then((data) => {
-      setCurrentUser(data)
+      setCurrentUser((prevUsers) => [...prevUsers, ...data])
     }).catch((err) => {
       console.log("The error is: ", err)
     })
+  }
+
+  useEffect(() => {
+    fetchData()
   }, []);
 
-  const testDatabase = () => {
-    fetch('http://localhost:3000/api/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(testData)
-    }).then((res) => {
-      console.log("Data saved successfully")
-    }).catch((err) => {
-      console.log("Error saving to database: ", err)
-    })
+  const testDatabase = async () => {
+    try {
+      const newUser = await fetch('http://localhost:3000/api/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(testData)
+      });
+
+      fetchData()
+    } catch (error) {
+      console.log("Error saving to database: ", error)
+    }
   }
 
   return (
     <>
-      <h1>Username: {currentUser}</h1>
+      <h1>Usernames:</h1>
+      {currentUser.map((user, index) => (
+        <p key={index}>{user.name}</p>
+      ))}
       <Button onClick={testDatabase}>Click to test database</Button>
     </>
   )
