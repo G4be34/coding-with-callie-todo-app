9ef44db714a9ed27a7c516c52d5bc856dd147a62
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-01-30 18:17:44 -0700

    Add in logout functionality and adjust header styles

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -1,5 +1,7 @@
-import { Avatar, Button, Flex, Image, Link, Spacer } from "@chakra-ui/react"
-import { Link as ReactRouterLink } from "react-router-dom"
+import { Avatar, Button, Flex, Image, Link, Spacer } from "@chakra-ui/react";
+import { useState } from "react";
+import { Link as ReactRouterLink } from "react-router-dom";
+import { useAuth } from "../context/AuthProvider";
 
 const links = [
   { label: 'Todos', path: '/' },
@@ -8,16 +10,26 @@ const links = [
 ]
 
 export const Header = () => {
+  const { logoutUser } = useAuth();
+  const [showOptions, setShowOptions] = useState(false);
+
   return (
-    <Flex as={"header"} borderBottom={"1px solid black"} p={4} alignItems={"center"}>
+    <Flex as={"header"} borderBottom={"1px solid black"} p={4} px={14} alignItems={"center"}>
       <Image src="/logo.jpeg" alt="logo" boxSize="50px" objectFit={"contain"}/>
       <Spacer/>
       {links.map((link, idx) => (
         <Link as={ReactRouterLink} to={link.path} key={idx}>
-          <Button ml={idx === 0 ? 0 : 2}>{link.label}</Button>
+          <Button ml={idx === 0 ? 0 : 6}>{link.label}</Button>
         </Link>
       ))}
-      <Avatar name="test" src="https://bit.ly/dan-abramov" ml={10} />
+      <Avatar name="test" src="https://bit.ly/dan-abramov" cursor={"pointer"} onClick={() => setShowOptions(!showOptions)} ml={10} pos={"relative"} />
+      {showOptions ?
+        <Flex flexDir={"column"} pos={"absolute"} right={5} top={20} border={"1px solid black"} borderRadius={10} bgColor={"white"} p={4} gap={2}>
+          <Button>Settings</Button>
+          <Button onClick={logoutUser} >Logout</Button>
+        </Flex>
+        : null
+      }
     </Flex>
   )
 }
