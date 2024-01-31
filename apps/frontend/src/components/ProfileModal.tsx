be35fd3e4cad02f610211385de66a607362d335d
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-01-31 04:51:37 -0700

    Add basic functionality to profile modal

--- apps/frontend/src/components/ProfileModal.tsx
+++ apps/frontend/src/components/ProfileModal.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonGroup, Editable, EditableInput, EditablePreview, Flex, IconButton, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useEditableControls } from "@chakra-ui/react";
+import { Button, ButtonGroup, Editable, EditableInput, EditablePreview, Flex, Heading, IconButton, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useEditableControls } from "@chakra-ui/react";
 import { useState } from "react";
 import { AiFillCloseSquare } from "react-icons/ai";
 import { FaEdit } from "react-icons/fa";
@@ -12,17 +12,17 @@ export const ProfileModal = ({ setShowModal, showModal }) => {
   const [currentTheme, setCurrentTheme] = useState("test");
   const [currentFont, setCurrentFont] = useState("test");
 
+  const saveEdit = () => {
+    console.log("Saved: ", currentName, currentEmail, currentPw, currentTheme, currentFont);
+  }
+
   const EditableControls = () => {
 
     const { isEditing, getSubmitButtonProps, getCancelButtonProps, getEditButtonProps } = useEditableControls();
 
-    const saveEdit = () => {
-      console.log("Saved: ", currentName, currentEmail, currentPw, currentTheme, currentFont);
-    }
-
     return isEditing ? (
       <ButtonGroup>
-        <IconButton icon={<ImCheckboxChecked />} aria-label="Save" {...getSubmitButtonProps()} onClick={saveEdit} />
+        <IconButton icon={<ImCheckboxChecked />} aria-label="Save" {...getSubmitButtonProps()} />
         <IconButton icon={<AiFillCloseSquare />} aria-label="Cancel" {...getCancelButtonProps()} />
       </ButtonGroup>
     ) : (
@@ -33,34 +33,37 @@ export const ProfileModal = ({ setShowModal, showModal }) => {
   }
 
   return (
-    <Modal isOpen={showModal} onClose={() => setShowModal(false)} isCentered>
+    <Modal isOpen={showModal} onClose={() => setShowModal(false)} isCentered size={"2xl"}>
       <ModalOverlay />
-      <ModalContent display={"flex"} flexDir={"row"}>
+      <ModalContent display={"flex"} flexDir={"row"} >
         <Flex flexDir={"column"} gap={4} justifyContent={"center"} borderRight={"1px solid black"}>
           <Button variant={"ghost"} onClick={() => setCurrentTab("Profile")}>Profile Settings</Button>
           <Button variant={"ghost"} onClick={() => setCurrentTab("Theme")}>Color Themes</Button>
           <Button variant={"ghost"} onClick={() => setCurrentTab("Font")}>Fonts Styles</Button>
         </Flex>
-        <Flex flexDir={"column"}>
-          <ModalHeader>{currentTab}</ModalHeader>
-          <ModalBody gap={4}>
+        <Flex flexDir={"column"} flex={1}>
+          <ModalHeader textDecoration={"underline"}>{currentTab}</ModalHeader>
+          <ModalBody gap={6} display={"flex"} flexDir={"column"} alignItems={"center"}>
             {currentTab === "Profile" ?
               <>
-                <Editable defaultValue={currentName} isPreviewFocusable={false} display={"flex"}>
-                  <EditablePreview />
-                  <Input as={EditableInput} />
+                <Heading size={"md"} mb={-2}>Username:</Heading>
+                <Editable defaultValue={currentName} isPreviewFocusable={false} display={"flex"} onChange={(e) => setCurrentName(e)} onSubmit={() => saveEdit()} >
+                  <EditablePreview w={"300px"} />
+                  <Input as={EditableInput} w={"300px"} mr={12} />
                   <EditableControls />
                 </Editable>
 
-                <Editable defaultValue={currentEmail} isPreviewFocusable={false} display={"flex"}>
-                  <EditablePreview />
-                  <Input as={EditableInput} />
+                <Heading size={"md"} mb={-2}>Email:</Heading>
+                <Editable defaultValue={currentEmail} isPreviewFocusable={false} display={"flex"} onChange={(e) => setCurrentEmail(e)} >
+                  <EditablePreview w={"300px"} />
+                  <Input as={EditableInput} w={"300px"} mr={12} />
                   <EditableControls />
                 </Editable>
 
-                <Editable defaultValue={currentPw} isPreviewFocusable={false} display={"flex"}>
-                  <EditablePreview />
-                  <Input as={EditableInput} />
+                <Heading size={"md"} mb={-2}>Password:</Heading>
+                <Editable defaultValue={currentPw} isPreviewFocusable={false} display={"flex"} onChange={(e) => setCurrentPw(e)} >
+                  <EditablePreview w={"300px"} />
+                  <Input as={EditableInput} w={"300px"} mr={12} />
                   <EditableControls />
                 </Editable>
               </>
@@ -68,9 +71,10 @@ export const ProfileModal = ({ setShowModal, showModal }) => {
             }
             {currentTab === "Theme" ?
               <>
-                <Editable defaultValue={currentTheme} isPreviewFocusable={false}>
-                  <EditablePreview />
-                  <Input as={EditableInput} />
+                <Heading size={"md"} mb={-2}>Current Theme:</Heading>
+                <Editable defaultValue={currentTheme} isPreviewFocusable={false} display={"flex"} onChange={(e) => setCurrentTheme(e)} >
+                  <EditablePreview w={"300px"} />
+                  <Input as={EditableInput} w={"300px"} mr={12} />
                   <EditableControls />
                 </Editable>
               </>
@@ -78,9 +82,10 @@ export const ProfileModal = ({ setShowModal, showModal }) => {
             }
             {currentTab === "Font" ?
               <>
-                <Editable defaultValue={currentFont} isPreviewFocusable={false}>
-                  <EditablePreview />
-                  <Input as={EditableInput} />
+                <Heading size={"md"} mb={-2}>Current Font:</Heading>
+                <Editable defaultValue={currentFont} isPreviewFocusable={false} display={"flex"} onChange={(e) => setCurrentFont(e)} >
+                  <EditablePreview w={"300px"} />
+                  <Input as={EditableInput} w={"300px"} mr={12} />
                   <EditableControls />
                 </Editable>
               </>
