c3e85b4697faea22eccc7272ef144820518ad806
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-02-03 02:59:25 -0700

    Complete error handling for sign up page

--- apps/frontend/src/auth/signup-page/SignUpPage.tsx
+++ apps/frontend/src/auth/signup-page/SignUpPage.tsx
@@ -7,6 +7,7 @@ export const SignUpPage = () => {
   const navigate = useNavigate();
   const [existingUser, setExistingUser] = useState(false);
   const [email, setEmail] = useState('');
+  const [invalidEmail, setInvalidEmail] = useState(false);
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [username, setUsername] = useState('');
@@ -38,7 +39,12 @@ export const SignUpPage = () => {
       navigate('/login');
     } catch (error) {
       if (isAxiosError(error) && error.response && error.response.status === 409) {
+        setInvalidEmail(false);
         setExistingUser(true);
+      }
+
+      if (isAxiosError(error) && error.response && error.response.data.message.includes('email must be an email')) {
+        setInvalidEmail(true);
       }
       console.log("Error creating user: ", error);
     }
@@ -48,15 +54,17 @@ export const SignUpPage = () => {
     <Flex flexDir={"column"} justifyContent={"center"} alignItems={"center"} h={"100vh"} bgColor={"gray.300"} pos={"relative"}>
       <Heading position={"absolute"} top={"15%"}>Sign up for CWC Todo App</Heading>
       <Flex flexDir={"column"} as="form" border={"1px solid black"} p={6} w={"450px"} borderRadius={10} rowGap={8} bgColor={"white"} mb={4}>
-        <FormControl isRequired>
+        <FormControl isRequired isInvalid={username.length < 3}>
           <FormLabel>Username</FormLabel>
           <Input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+          {username.length < 3 ? <FormErrorMessage>Username must be at least 3 characters long</FormErrorMessage> : null}
         </FormControl>
 
         <FormControl isRequired isInvalid={existingUser}>
           <FormLabel>Email</FormLabel>
           <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
           {existingUser ? <FormErrorMessage>Email already exists</FormErrorMessage> : null}
+          {invalidEmail ? <Text mt={2} fontSize={"sm"} color={"red"}>Email must be a valid email address</Text > : null}
         </FormControl>
 
         <FormControl isRequired isInvalid={password.length < 6}>
@@ -71,7 +79,7 @@ export const SignUpPage = () => {
           {!pwMatch ? <FormErrorMessage>Passwords do not match</FormErrorMessage> : null}
         </FormControl>
 
-        <Button onClick={createUser}>Sign up</Button>
+        <Button onClick={createUser} isDisabled={username.length < 3 || password.length < 6}>Sign up</Button>
       </Flex>
       <Text>Already have an account? <Link as={ReactRouterLink} to="/" color={"#209CF0"} marginLeft={2}>Login instead</Link></Text>
     </Flex>
