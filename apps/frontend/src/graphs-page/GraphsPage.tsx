f4f3947cf0dcf5f496f4a62da9d9092416c0c6fd
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-08-14 17:24:20 -0600

    Add line chart to graphs page

--- apps/frontend/src/graphs-page/GraphsPage.tsx
+++ apps/frontend/src/graphs-page/GraphsPage.tsx
@@ -1,6 +1,6 @@
 import { Box, Grid } from "@chakra-ui/react";
 import { useLoaderData } from "react-router-dom";
-import { Bar, BarChart, CartesianGrid, Legend, Rectangle, Tooltip, XAxis, YAxis } from "recharts";
+import { Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Rectangle, Tooltip, XAxis, YAxis } from "recharts";
 
 export const GraphsPage = () => {
   const { barChartData } = useLoaderData();
@@ -15,7 +15,7 @@ export const GraphsPage = () => {
       width={"100vw"}
       overflow={"auto"}
     >
-      <Box height="auto" margin={"auto"} width="auto" gridColumn="1" gridRow="1">
+      <Box margin={"auto"} gridColumn="1" gridRow="1">
         <BarChart
           width={400}
           height={300}
@@ -29,7 +29,20 @@ export const GraphsPage = () => {
           <Bar dataKey="completed" fill="#8884d8" activeBar={<Rectangle fill="gold" stroke="black" />} />
         </BarChart>
       </Box>
-      <Box bg="green" height="100px" margin={"auto"} width="100px" gridColumn="2" gridRow="1" />
+      <Box margin={"auto"} gridColumn="2" gridRow="1">
+        <LineChart
+          width={400}
+          height={300}
+          data={barChartData}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="week" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="completed" stroke="#8884d8" activeDot={{ r: 8 }} />
+        </LineChart>
+      </Box>
       <Box bg="blue" height="100px" margin={"auto"} width="100px" gridColumn="1" gridRow="2" />
       <Box bg="orange" height="100px" margin={"auto"} width="100px" gridColumn="2" gridRow="2" />
       <Box bg="purple" height="100px" margin={"auto"} width="100px" gridColumn="1 / 3" gridRow="3" />
