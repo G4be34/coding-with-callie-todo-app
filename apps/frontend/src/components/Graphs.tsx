4531659396a95d1763b02257941155aabd2f6cea
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-08-27 03:15:49 -0600

    Add multi graph functionality and move graphs into their own file

---
+++ apps/frontend/src/components/Graphs.tsx
@@ -0,0 +1,286 @@
+import { Flex, Select, Text } from "@chakra-ui/react";
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Cell, Legend, Line, LineChart, Pie, PieChart, Rectangle, Tooltip, TooltipProps, XAxis, YAxis } from "recharts";
+import { NameType, ValueType } from "recharts/types/component/DefaultTooltipContent";
+
+
+type CustomizedLabelProps = {
+  cx: number;
+  cy: number;
+  midAngle: number;
+  innerRadius: number;
+  outerRadius: number;
+  percent: number;
+  index: number;
+};
+
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }: CustomizedLabelProps) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor="middle" dominantBaseline="middle">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+
+const CustomTooltip = ({ active, payload, label }: TooltipProps<ValueType, NameType>) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip" style={{ backgroundColor: '#fff', padding: '10px', border: '1px solid #ccc' }}>
+        <p className="label">{`${label}`}</p>
+        <p className="intro" style={{ color: 'red' }}>{`Highest: ${payload[0] ? payload[0].value : 0}`}</p>
+        <p className="intro" style={{ color: 'orange' }}>{`High: ${payload[1] ? payload[1].value : 0}`}</p>
+        <p className="intro" style={{ color: 'gray' }}>{`Normal: ${payload[2] ? payload[2].value : 0}`}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+
+export const BarGraph = () => {
+  const { barChartData } = useLoaderData() as { barChartData: { week: string, completed: number }[] };
+  const [graphType, setGraphType] = useState('bar');
+
+  return (
+    <>
+      <Flex justifyContent={"flex-end"} ml={"auto"} alignItems={"center"} maxW={"90%"}>
+        <Text flex={1} fontWeight={"bold"} fontSize={"xl"} textAlign={"center"}>
+          Completed Tasks
+        </Text>
+        <Select
+          size={"xs"}
+          defaultValue={"bar"}
+          maxW={"100px"}
+          onChange={(e) => setGraphType(e.target.value)}
+          >
+          <option value="bar">Bar</option>
+          <option value="line">Line</option>
+        </Select>
+      </Flex>
+
+      {graphType === 'bar'
+        ? <>
+            <BarChart
+              width={500}
+              height={400}
+              data={barChartData}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="week" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="completed" fill="#8884d8" activeBar={<Rectangle fill="gold" stroke="black" />} />
+            </BarChart>
+          </>
+        : <>
+            <LineChart
+              width={500}
+              height={400}
+              data={barChartData}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="week" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="completed" stroke="#8884d8" activeDot={{ r: 8 }} />
+            </LineChart>
+          </>
+      }
+    </>
+  )
+};
+
+
+export const PieGraph = () => {
+  const { pieChartData } = useLoaderData() as { pieChartData: { name: string, value: number }[] };
+  const [graphType, setGraphType] = useState('pie');
+
+  return (
+    <>
+      {graphType === 'pie'
+        ? <>
+            <Select
+              ml={"auto"}
+              size={"xs"}
+              defaultValue={"pie"}
+              maxW={"100px"}
+              onChange={(e) => setGraphType(e.target.value)}
+              >
+              <option value="pie">Pie</option>
+              <option value="bar">Bar</option>
+            </Select>
+            <PieChart width={300} height={300}>
+              <Pie
+                data={pieChartData}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="value"
+              >
+                <Cell key={"normal"} fill={"gray"} />
+                <Cell key={"highest"} fill={"red"} />
+                <Cell key={"high"} fill={"orange"} />
+              </Pie>
+              <Tooltip />
+            </PieChart>
+            <Text fontWeight={"bold"} fontSize={"xl"} textAlign={"center"}>
+              Number of tasks by priority
+            </Text>
+          </>
+        : <>
+            <Flex justifyContent={"flex-end"} ml={"auto"} alignItems={"center"} maxW={"90%"}>
+              <Text flex={1} fontWeight={"bold"} fontSize={"xl"} textAlign={"center"}>
+                Number of tasks by priority
+              </Text>
+              <Select
+                size={"xs"}
+                defaultValue={"pie"}
+                maxW={"100px"}
+                onChange={(e) => setGraphType(e.target.value)}
+                >
+                <option value="pie">Pie</option>
+                <option value="bar">Bar</option>
+              </Select>
+            </Flex>
+            <BarChart
+              width={500}
+              height={400}
+              data={pieChartData}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="value" fill="#8884d8" activeBar={<Rectangle fill="gold" stroke="black" />} />
+            </BarChart>
+          </>
+      }
+    </>
+  )
+};
+
+
+export const StackedBarGraph = () => {
+  const { stackedBarChartData } = useLoaderData() as { stackedBarChartData: { week: string, normal: number, high: number, highest: number }[] };
+  const [graphType, setGraphType] = useState('bar');
+
+  return (
+    <>
+      <Flex justifyContent={"flex-end"} ml={"auto"} alignItems={"center"} maxW={"90%"}>
+        <Text flex={1} fontWeight={"bold"} fontSize={"xl"} textAlign={"center"}>
+          Tasks created each week
+        </Text>
+        <Select
+          size={"xs"}
+          defaultValue={"bar"}
+          maxW={"100px"}
+          onChange={(e) => setGraphType(e.target.value)}
+          >
+          <option value="bar">Bar</option>
+          <option value="area">Area</option>
+        </Select>
+      </Flex>
+
+      {graphType === 'bar'
+        ? <BarChart
+            width={500}
+            height={400}
+            data={stackedBarChartData}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="week" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="Normal" fill="gray" stackId={"a"} activeBar={<Rectangle fill="gray" stroke="black" />} />
+            <Bar dataKey="High" fill="orange" stackId={"a"} activeBar={<Rectangle fill="orange" stroke="black" />} />
+            <Bar dataKey="Highest" fill="red" stackId={"a"} activeBar={<Rectangle fill="red" stroke="black" />} />
+          </BarChart>
+        : <AreaChart
+            width={500}
+            height={400}
+            data={stackedBarChartData}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="week" />
+            <YAxis />
+            <Tooltip content={<CustomTooltip />} />
+            <Legend />
+            <Area type="monotone" dataKey="Highest" stroke="white" fill="red" stackId={"b"} activeDot={{ r: 8 }} />
+            <Area type="monotone" dataKey="High" stroke="white" fill="orange" stackId={"b"} activeDot={{ r: 8 }} />
+            <Area type="monotone" dataKey="Normal" stroke="white" fill="gray" stackId={"b"} activeDot={{ r: 8 }} />
+          </AreaChart>
+      }
+    </>
+  )
+};
+
+
+export const AreaGraph = () => {
+  const { areaChartData } = useLoaderData() as { areaChartData: { week: string, Highest: number, High: number, Normal: number }[] };
+  const [graphType, setGraphType] = useState('area');
+
+  return (
+    <>
+      <Flex justifyContent={"flex-end"} ml={"auto"} alignItems={"center"} maxW={"90%"}>
+        <Text flex={1} fontWeight={"bold"} fontSize={"xl"} textAlign={"center"}>
+          Task average time to complete
+        </Text>
+        <Select
+          size={"xs"}
+          defaultValue={"bar"}
+          maxW={"100px"}
+          onChange={(e) => setGraphType(e.target.value)}
+          >
+          <option value="area">Area</option>
+          <option value="line">Line</option>
+        </Select>
+      </Flex>
+
+      {graphType === 'area'
+        ? <AreaChart
+            width={500}
+            height={400}
+            data={areaChartData}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="week" />
+            <YAxis />
+            <Tooltip content={<CustomTooltip />}/>
+            <Legend />
+            <Area type="monotone" dataKey="Highest" stroke="white" fill="red" stackId={"b"} activeDot={{ r: 8 }} />
+            <Area type="monotone" dataKey="High" stroke="white" fill="orange" stackId={"b"} activeDot={{ r: 8 }} />
+            <Area type="monotone" dataKey="Normal" stroke="white" fill="gray" stackId={"b"} activeDot={{ r: 8 }} />
+          </AreaChart>
+        : <LineChart
+            width={500}
+            height={400}
+            data={areaChartData}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="week" />
+            <YAxis />
+            <Tooltip />
+            <Line type="monotone" dataKey="Normal" stroke="gray" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="High" stroke="orange" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="Highest" stroke="red" activeDot={{ r: 8 }} />
+          </LineChart>
+      }
+    </>
+  )
+};
--- apps/frontend/src/graphs-page/GraphsPage.tsx
+++ apps/frontend/src/graphs-page/GraphsPage.tsx
@@ -1,77 +1,16 @@
 import { Box, Grid, Text } from "@chakra-ui/react";
 import { useLoaderData } from "react-router-dom";
-import { Area, AreaChart, Bar, BarChart, CartesianGrid, Cell, Legend, Pie, PieChart, Rectangle, Tooltip, TooltipProps, XAxis, YAxis } from "recharts";
-import { NameType, ValueType } from 'recharts/types/component/DefaultTooltipContent';
+import { AreaGraph, BarGraph, PieGraph, StackedBarGraph } from "../components/Graphs";
 
 
 type GraphsDataType = {
-  barChartData: {
-    week: string
-    completed: number
-  }[],
-  areaChartData: {
-    week: string
-    Normal: number
-    High: number
-    Highest: number
-  }[],
-  pieChartData: {
-    name: string
-    value: number
-  }[],
-  stackedBarChartData: {
-    week: string
-    Normal: number
-    High: number
-    Highest: number
-  }[],
   numOfIncomplete: number,
   numOfOverdue: number
 };
 
-type CustomizedLabelProps = {
-  cx: number;
-  cy: number;
-  midAngle: number;
-  innerRadius: number;
-  outerRadius: number;
-  percent: number;
-  index: number;
-};
-
-
-const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }: CustomizedLabelProps) => {
-  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-  const x = cx + radius * Math.cos(-midAngle * RADIAN);
-  const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-  return (
-    <text x={x} y={y} fill="white" textAnchor="middle" dominantBaseline="middle">
-      {`${(percent * 100).toFixed(0)}%`}
-    </text>
-  );
-};
-
-
-const CustomTooltip = ({ active, payload, label }: TooltipProps<ValueType, NameType>) => {
-  if (active && payload && payload.length) {
-    return (
-      <div className="custom-tooltip" style={{ backgroundColor: '#fff', padding: '10px', border: '1px solid #ccc' }}>
-        <p className="label">{`${label}`}</p>
-        <p className="intro" style={{ color: 'red' }}>{`Highest: ${payload[0].value}`}</p>
-        <p className="intro" style={{ color: 'orange' }}>{`High: ${payload[1].value}`}</p>
-        <p className="intro" style={{ color: 'gray' }}>{`Normal: ${payload[2].value}`}</p>
-      </div>
-    );
-  }
-
-  return null;
-};
-
 
 export const GraphsPage = () => {
-  const { barChartData, pieChartData, stackedBarChartData, areaChartData, numOfIncomplete, numOfOverdue } = useLoaderData() as GraphsDataType;
+  const { numOfIncomplete, numOfOverdue } = useLoaderData() as GraphsDataType;
 
   return (
     <Box height={"100vh"} width={"100vw"} display="flex" justifyContent="center" alignItems="center" overflowY={"auto"}>
@@ -92,73 +31,16 @@ export const GraphsPage = () => {
           <Text fontWeight={"bold"} fontSize={["s", "s", "xl"]}>Overdue Tasks: {numOfOverdue}</Text>
         </Box>
         <Box gridColumn="1" gridRow={[1, 1, 1]} mt={["5%", "5%", 0]}>
-          <Text fontWeight={"bold"} fontSize={"xl"} textAlign={"center"}>Completed Tasks</Text>
-          <BarChart
-            width={500}
-            height={400}
-            data={barChartData}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="week" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="completed" fill="#8884d8" activeBar={<Rectangle fill="gold" stroke="black" />} />
-          </BarChart>
+          <BarGraph />
         </Box>
         <Box gridColumn={[1, 1, 2]} gridRow={[2, 2, 1]}>
-          <PieChart width={300} height={300}>
-            <Pie
-              data={pieChartData}
-              cx="50%"
-              cy="50%"
-              labelLine={false}
-              label={renderCustomizedLabel}
-              outerRadius={80}
-              fill="#8884d8"
-              dataKey="value"
-            >
-              <Cell key={"normal"} fill={"gray"} />
-              <Cell key={"highest"} fill={"red"} />
-              <Cell key={"high"} fill={"orange"} />
-            </Pie>
-            <Tooltip />
-          </PieChart>
-          <Text fontWeight={"bold"} fontSize={"xl"} textAlign={"center"}>Number of tasks by priority</Text>
+          <PieGraph />
         </Box>
         <Box gridColumn="1" gridRow={[3, 3, 2]} mb={"auto"} pb={[0, 0, "15%"]}>
-          <Text fontWeight={"bold"} fontSize={"xl"} textAlign={"center"}>Tasks created each week</Text>
-          <BarChart
-            width={500}
-            height={400}
-            data={stackedBarChartData}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="week" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="Normal" fill="gray" stackId={"a"} activeBar={<Rectangle fill="gray" stroke="black" />} />
-            <Bar dataKey="High" fill="orange" stackId={"a"} activeBar={<Rectangle fill="orange" stroke="black" />} />
-            <Bar dataKey="Highest" fill="red" stackId={"a"} activeBar={<Rectangle fill="red" stroke="black" />} />
-          </BarChart>
+          <StackedBarGraph />
         </Box>
         <Box gridColumn={[1, 1, 2]} gridRow={[4, 4, 2]} pb={["20%", "20%", "15%"]} mb={"auto"}>
-          <Text fontWeight={"bold"} fontSize={"xl"} textAlign={"center"}>Task average time to complete</Text>
-          <AreaChart
-            width={500}
-            height={400}
-            data={areaChartData}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="week" />
-            <YAxis />
-            <Tooltip content={<CustomTooltip />}/>
-            <Legend />
-            <Area type="monotone" dataKey="Highest" stroke="white" fill="red" stackId={"b"} activeDot={{ r: 8 }} />
-            <Area type="monotone" dataKey="High" stroke="white" fill="orange" stackId={"b"} activeDot={{ r: 8 }} />
-            <Area type="monotone" dataKey="Normal" stroke="white" fill="gray" stackId={"b"} activeDot={{ r: 8 }} />
-          </AreaChart>
+          <AreaGraph />
         </Box>
       </Grid>
     </Box>
