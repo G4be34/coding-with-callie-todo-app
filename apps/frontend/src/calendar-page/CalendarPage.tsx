bd1d2f3711e32c0d3993ed3b295f1a4cc1dfdcac
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-08-28 03:44:26 -0600

    Install fullcalendar library and create basic calendar page with custom css

--- apps/frontend/src/calendar-page/CalendarPage.tsx
+++ apps/frontend/src/calendar-page/CalendarPage.tsx
@@ -1,7 +1,20 @@
+import { Flex } from '@chakra-ui/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import FullCalendar from '@fullcalendar/react';
+import './calendarpage.css';
+
 export const CalendarPage = () => {
   return (
-    <div>
-      <h1>Calendar Page</h1>
-    </div>
+    <Flex flex={1} p={5}>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        handleWindowResize
+        events={[
+          { title: 'event 1', date: '2024-08-01' },
+          { title: 'event 2', date: '2024-08-02' },
+        ]}
+      />
+    </Flex>
   )
 }
