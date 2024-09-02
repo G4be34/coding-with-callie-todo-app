8224f657537308f4d2c3936e684bcd595639f640
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-09-02 04:24:23 -0600

    Add basic layout for event modal and most functionality to updating event description

--- apps/frontend/src/calendar-page/CalendarPage.tsx
+++ apps/frontend/src/calendar-page/CalendarPage.tsx
@@ -1,9 +1,9 @@
-import { Flex, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay, useToast } from '@chakra-ui/react';
+import { Editable, EditableInput, EditablePreview, Flex, Modal, ModalBody, ModalContent, ModalOverlay, Select, Text, useToast } from '@chakra-ui/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import FullCalendar from '@fullcalendar/react';
 import axios from 'axios';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import './calendarpage.css';
 
@@ -11,8 +11,8 @@ import './calendarpage.css';
 type CalendarDataType = {
   title: string;
   date: string;
-  description: string;
   priority: string;
+  id: string;
 };
 
 type LoaderDataType = {
@@ -25,6 +25,7 @@ export const CalendarPage = () => {
   const toast = useToast();
   const [showTaskModal, setShowTaskModal] = useState(false);
   const [selectedEvent, setSelectedEvent] = useState<CalendarDataType | null>(null);
+  const calendarRef = useRef<FullCalendar | null>(null);
 
   const updateDueDate = async (info) => {
     const newDate = new Date(info.event.start).getTime();
@@ -54,10 +55,50 @@ export const CalendarPage = () => {
 
   const handleEventClick = (info) => {
     const { title, extendedProps } = info.event;
-    const { description, priority } = extendedProps;
+    const { priority } = extendedProps;
+    const { id } = info.event;
+    const { date } = info.event;
 
-    setSelectedEvent({ title, description, priority, date: info.event.date });
+    setSelectedEvent({ title, priority, date, id });
     setShowTaskModal(true);
+  };
+
+  const changeDescription = async (description: string) => {
+    try {
+      await axios.patch(`/api/todos/${selectedEvent?.id}`, { description }, { headers: { Authorization: `Bearer ${access_token}` } });
+
+      setSelectedEvent((prev) => prev ? { ...prev, title: description } : prev);
+
+      const calendarApi = calendarRef.current?.getApi();
+      if (selectedEvent) {
+        const calendarEvent = calendarApi?.getEventById(selectedEvent.id);
+        if (calendarEvent) {
+          calendarEvent.setProp('title', description);
+        }
+      }
+
+      toast({
+        title: "Description has been updated",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+        position: "top",
+      });
+    } catch (error) {
+      console.error("Error changing description:", error);
+      toast({
+        title: "Error changing description",
+        description: "Please try again",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+        position: "top",
+      });
+    }
+  };
+
+  const changePriority = async (priority: string) => {
+
   };
 
 
@@ -67,11 +108,36 @@ export const CalendarPage = () => {
         ? <Modal isOpen={showTaskModal} onClose={() => setShowTaskModal(false)} isCentered size={"md"}>
             <ModalOverlay />
             <ModalContent>
-              <ModalHeader>
-                {selectedEvent?.title}
-              </ModalHeader>
               <ModalBody>
-                {selectedEvent?.priority}
+                <Flex>
+                  <Text>Description: </Text>
+                  <Editable
+                    textAlign="center"
+                    cursor={"pointer"}
+                    w={"100%"}
+                    onSubmit={changeDescription}
+                    defaultValue={selectedEvent?.title}
+                  >
+                    <EditablePreview />
+                    <EditableInput />
+                  </Editable>
+                </Flex>
+                <Flex>
+                  <Text>Priority: </Text>
+                  <Select
+                    cursor={"pointer"}
+                    size={"sm"}
+                    defaultValue={selectedEvent?.priority}
+                    bg={selectedEvent?.priority === 'Highest' ? 'red' : selectedEvent?.priority === 'High' ? 'orange' : 'gray'}
+                    color={selectedEvent?.priority === 'High' ? 'black' : 'white'}
+                    variant={"filled"}
+                    onChange={(e) => changePriority(e.target.value)}
+                  >
+                    <option value="Highest">Highest</option>
+                    <option value="High">High</option>
+                    <option value="Normal">Normal</option>
+                  </Select>
+                </Flex>
               </ModalBody>
             </ModalContent>
           </Modal>
